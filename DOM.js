b9e8f9960b9c3ea8f63fcbdc2f4f954808640c26
faisalzahoor01 <faisalzahoor96@gmail.com>
2024-08-20 16:54:09 +0500

    8/20/24

--- DOM.js
+++ DOM.js
@@ -1,3 +1,23 @@
-let div = document.querySelector(".box")
-console.dir(div)
-div.innerText = "Hello boy"
+// let div1 = document.querySelector(".box")
+// console.dir(div1)
+// // div.innerText = "Hello boy"
+// // let child1 = document.querySelector("div")
+// // console.dir(child1.firstChild)
+// // child1.innerText = "hello child"
+// console.dir(div.children)
+
+// let div = document.querySelectorAll(".box")
+// console.dir(div)
+// let idx = 1
+// for (let box of div) {
+//     box.innerText = `hello ${idx}`
+//     idx++
+// }
+
+// document.body.style.backgroundColor = "green"
+
+let div = document.querySelector("div")
+console.log(div.setAttribute("class","RAMI BOX")) 
+
+
+
