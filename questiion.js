4cdaa48bebc3673e216a86a9e7c6ad57ae54bce2
faisalzahoor01 <faisalzahoor96@gmail.com>
2024-08-20 17:51:21 +0500

    new commit

--- questiion.js
+++ questiion.js
@@ -4,3 +4,9 @@ nbutton.style.backgroundColor = "red";
 nbutton.style.color= "white";
 
 document.querySelector("body").prepend(nbutton)
+
+// qs:2
+
+let para = document.querySelector("p");
+
+para.classList.add("para2");
