23b74f53523bb31aa12a78bc407dead48cad94ed
faisalzahoor01 <faisalzahoor96@gmail.com>
2024-08-20 17:31:02 +0500

    Question solving

---
+++ questiion.js
@@ -0,0 +1,6 @@
+let nbutton = document.createElement("button");
+nbutton.innerText = "Click me!";
+nbutton.style.backgroundColor = "red";
+nbutton.style.color= "white";
+
+document.querySelector("body").prepend(nbutton)
